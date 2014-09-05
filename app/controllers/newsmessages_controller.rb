09e69d224e9bef93fc03f279bd6f93b5ac1ae098
Tristan Hisgen <tristan.hisgen@mni.thm.de>
2014-09-05 16:27:32 +0200

    build helper

--- app/controllers/newsmessages_controller.rb
+++ app/controllers/newsmessages_controller.rb
@@ -1,9 +1,7 @@
 class NewsmessagesController < ApplicationController
 	def index
-		@listener = Listener.all.map(&:url)
-		urls = getListeners
-		feeds = Feedjira::Feed.fetch_and_parse urls 
-		@feed = feeds['http://www.ardmediathek.de/tv?rss']
+		listener = Listener.all.map(&:url)
+		:feeds = Feedjira::Feed.fetch_and_parse @listener
 	#	@feed = feeds['http://heise.de.feedsportal.com/c/35207/f/653901/index.rss']
 	end
 
@@ -23,12 +21,6 @@ class NewsmessagesController < ApplicationController
 	private
 	def newsmessage_params
 		params.require('newsmessage').permit('title','url','comment','image')
-	end
-
-	def getListeners
-		%w[http://gdata.youtube.com/feeds/base/users/gronkh/uploads?v=2&alt=rss&client=ytapi-youtube-rss-redirect&orderby=updated
-				http://heise.de.feedsportal.com/c/35207/f/653901/index.rss
-				http://www.ardmediathek.de/tv?rss]
 	end
 
 end
